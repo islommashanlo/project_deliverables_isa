2e405aeaa661749449e65266648b945c4d8d54bd
github username <github email address>
2020-08-04 16:27:28 -0400

    project_v005

--- run.rb
+++ run.rb
@@ -3,7 +3,8 @@ require_relative 'app/models/patients.rb'
 
 def doc_or_pat
     puts "Please, identify yourself (doctor/patient)"
-    if gets.strip == "doctor"
+    identity = gets.strip
+    if identity == "doctor"
         def run_doc
             puts "Please enter your name"
             name = gets.strip
@@ -12,21 +13,24 @@ def doc_or_pat
             puts "Please enter your years of practice"
             years = gets.strip.to_i
             user = Doctor.new(name, specialty, years)
+            puts "You are #{name}, your specialty is #{specialty} and you have #{years} years of practice"
         end
         run_doc
-    elsif gets.strip == "patient"
+    elsif identity == "patient"
         def run_pat
             puts "Please enter your name"
             name = gets.strip
             puts "Please enter your age"
             age = gets.strip
             user = Patient.new(name, age)
+            puts "You are #{name} and you are #{age} years old."
         end
         run_pat
     else
         puts "Please enter a valid response"
         doc_or_pat
     end
+
 end
 
 doc_or_pat
