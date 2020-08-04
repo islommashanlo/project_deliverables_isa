b3edcce1405ff4ebf6d1a31bacee044187ebfb07
github username <github email address>
2020-08-04 14:54:49 -0400

    project_v001

---
+++ app/models/doctors.rb
@@ -0,0 +1,12 @@
+class Doctor
+
+    attr_accessor :name, :years
+    attr_reader :specialty #att_reader allows us to "get" (i.e. read) the value but not change it
+
+    def initialize(name, specialty, years = 1)
+        @name = name
+        @specialty = specialty
+        @years = years
+    end
+    
+end
---
+++ app/models/patients.rb
@@ -0,0 +1,9 @@
+class Patients
+    attr_accessor :name, :age
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+end
